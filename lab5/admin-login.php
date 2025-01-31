c6b7595b567f8cfd5f2965ffbabfb40d4d293e03
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2025-01-31 11:59:49 +0530

    adding styles

--- lab5/admin-login.php
+++ lab5/admin-login.php
@@ -15,6 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo "<a href='studentreg.html' target='_blank'>Insert Student</a> <br/>";
         echo "<a href='update.html' target='_blank'>Update Student</a> <br/>";
         echo "<a href='delete.html' target='_blank'>Delete Student</a> <br/>";
+        echo "<a href='student.html' target='_blank'>View Student</a> <br/>";
     
     } else {
         echo "<h2 style='color: red; text-align: center;'>Invalid Username or Password.</h2>";
